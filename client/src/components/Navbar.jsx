7fdd05fe210259c18b6a39846513898fc1bc916e
sumit2719 <sumitsingh1339@gmail.com>
2024-01-31 22:05:12 +0530

    deployed ion netlify

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import './css/Navbar.css';
 
-function Navbar() {
+
+const Navbar = () => {
   return (
-    <div>
-     <h1>Navbar</h1> 
+    <div className="navbar-container">
+      <div className="logo">
+        <Link to={"/"} className="navbar-link">HOSTELIFY</Link>
+      </div>
+      <nav>
+        <ul className="navbar-list">
+          <li className="navbar-item"><Link to={"/"} className="navbar-link">Home</Link></li>
+          <li className="navbar-item"><Link to={"/about"} className="navbar-link">About</Link></li>
+          <li className="navbar-item"><Link to={"/login"} className="navbar-link">Login</Link></li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Navbar;
+
+
+
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Link, NavLink} from "react-router-dom";
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
+import About from './components/About.jsx';
+import Home from './components/Home.jsx'
+import Contact from './components/Contact.jsx';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <BrowserRouter>
-<Navbar/>
+<div><Navbar/></div>
 
-<App/>
+<Routes>
+  <Route path="/navbar" element={<p>hiiiiii</p>} />
+  <Route path="/footer" element={<App/>} />
+  <Route path="/about" element={<About/>} />
+  <Route path="/contact" element={<Contact/>} />  
+  <Route path="/" element={<Home/>} />
+</Routes>
 <Footer/>
 </BrowserRouter>
 );
