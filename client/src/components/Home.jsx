01b797ee7a675ad6a7d35a08e0ef645f429cbc36
sumit2719 <sumitsingh1339@gmail.com>
2024-02-02 15:31:49 +0530

    .

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import './css/home.css'; // Import the corresponding CSS file
+import './css/Home.css'; // Import the corresponding CSS file
 const Home = () => {
   const imageURL='https://scontent.fixc4-3.fna.fbcdn.net/v/t1.6435-9/93272843_101394028216928_5292488658975195136_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=7f8c78&_nc_ohc=7V1Zgxn4OAwAX9VO8cq&_nc_ht=scontent.fixc4-3.fna&oh=00_AfB4-b8_s_fGd-zQybMgArShOKwhHOfJSRCDDQ_fmPlZGw&oe=65E28221';
   return (
