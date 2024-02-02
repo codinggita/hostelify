3c0f74931d007b6b758835370cf1afc643b50a98
sumit2719 <sumitsingh1339@gmail.com>
2024-02-02 15:01:01 +0530

    updated homepage and css of some elements

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,12 +2,21 @@
 
 import React from 'react';
 import './css/home.css'; // Import the corresponding CSS file
-
 const Home = () => {
+  const imageURL='https://scontent.fixc4-3.fna.fbcdn.net/v/t1.6435-9/93272843_101394028216928_5292488658975195136_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=7f8c78&_nc_ohc=7V1Zgxn4OAwAX9VO8cq&_nc_ht=scontent.fixc4-3.fna&oh=00_AfB4-b8_s_fGd-zQybMgArShOKwhHOfJSRCDDQ_fmPlZGw&oe=65E28221';
   return (
     <div className="home-container">
-      <h1>Welcome to My Home Page</h1>
-      <p>This is a simple example of a home page in React.</p>
+      <div className='home-image'>
+        <img src={imageURL} alt="Hostel Image" className="home-image" />
+        <div className='overlay-text'>
+          <h1>Empower your hostel experience with seamless management</h1> 
+        </div>
+      </div>
+      <div className='site-images'>
+        <div className='site-text'>
+          Have a look at our hostels :
+        </div>
+      </div>
     </div>
   );
 };
